72fa086b2d13ab069fb709b608fb71d11609e60f
Huy Dinh <madhattervn@gmail.com>
2024-08-14 14:14:44 +0700

    test terraform v17

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,10 +19,10 @@ terraform {
 
 backend "remote" {
 		hostname = "app.terraform.io"
-		organization = "CloudQuickLabs"
+		organization = "HuyDinh"
 
 		workspaces {
-			name = "AWSEKS"
+			name = "java-devops"
 		}
 	}
 }
